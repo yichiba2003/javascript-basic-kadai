17eb0c2a8c10109723c2f070baf0a49523f3ac79
yichiba2003 <150329674+yichiba2003@users.noreply.github.com>
2023-12-21 18:47:36 -0800

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,20 @@
+const date = new Date();
+const [month, day, year] = [
+  newmonth = date.getMonth() + 1,
+  date.getDate (),
+  date.getFullYear(),
+];
+
+
+
+
+
+
+const [hour, minutes, seconds] = [
+  date.getHours(),
+  date.getMinutes(),
+  date.getSeconds(),
+];
+
+
+console.log (year + '年' + newmonth+ '月' + day + '日');
